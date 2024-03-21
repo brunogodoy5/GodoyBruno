5ba28d3a448ed3c44f96a79e1a1a0e2a3fa2c1df
brunogodoy5 <130806880+brunogodoy5@users.noreply.github.com>
2024-03-21 20:05:18 -0300

    Delete PROG directory

--- PROG/main.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "Persona.php";
-$persona1=new Persona("Javier","Parra",23333333);
-$persona2=new Persona("Pablo","Martinez",47723600);
-
-echo "¿Hola como te va?, ".$persona1->nombre." ".$persona1->apellido;
-echo $persona1->saludar();
