d4207b26de35cf5256f7f19910593df96e56cc97
brunogodoy5 <130806880+brunogodoy5@users.noreply.github.com>
2024-03-21 20:11:24 -0300

    Delete Tp1 directory

--- Tp1/js/funciones.js
+++
@@ -1,6 +0,0 @@
-function mifuncion() {
-    const aux=document.getElementById("apellido")
-    const aux1=document.getElementById("nombre")
-    const aux2=document.getElementById("edad")
-    alert(aux.value+" "+aux1.value+" "+aux2.value)
-} 
