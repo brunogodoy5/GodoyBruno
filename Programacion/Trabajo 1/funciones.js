ac78cdca4041382a41242fac236154da76ac46aa
Franco Robles <108472146+FranRob@users.noreply.github.com>
2024-04-04 21:13:37 -0300

    Trabajo 1 con funciones java

---
+++ Programacion/Trabajo 1/funciones.js
@@ -0,0 +1,17 @@
+
+function saludar(resultado){ 
+const aux=72;
+        if (resultado>aux) {
+            alert("El numero es mayor a la constante")
+        } else {
+            alert("El numero es menor a la constante")
+        }
+
+
+
+
+
+
+
+
+}
