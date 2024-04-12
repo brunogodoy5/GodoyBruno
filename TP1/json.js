b8c8cd8ebf92e2f9082586bf21ec84f6de6e2a78
GodoyBruno <bruno.gdoy19@gmail.com>
2024-04-12 16:14:14 -0300

    Ejercicio Comparar

--- TP1/json.js
+++ TP1/json.js
@@ -22,7 +22,102 @@ const personaje = {
     "url": "https://rickandmortyapi.com/api/character/140",
     "created": "2017-12-27T18:47:44.566Z"
 }
-document.getElementById("result").innerHTML=("El personaje " + personaje.name + " esta " + personaje.status);
+//document.getElementById("result").innerHTML=("El personaje " + personaje.name + " esta " + personaje.status);
+
+
+    /*forma para buscar usando la api chacarcter
+
+function buscarPersonaje(){
+  let codigoInput = document.getElementById("codigo").value;
+  document.getElementById("result").innerHTML=" ";
+  fetch("https://rickandmortyapi.com/api/character/" + codigoInput)
+  .then(Response => Response.json())
+  .then(data =>{
+  
+      if (data.name != null){
+          document.getElementById("result").innerHTML=("El personaje " + data.name + " esta " + data.status)
+  
+      }
+          else {
+          
+              document.getElementById("result").innerHTML=("El personaje no existe")   
+          }
+  
+      console.log(data)   
+  } )
+  }
+
+*/
+
+/* function buscarImagen(){
+    let codigoInput = document.getElementById("codigo").value;
+    
+    document.getElementById("result").innerHTML=" ";
+    fetch(`https://rickandmortyapi.com/api/character/${codigoInput}`)
+    .then(Response => Response.json())
+    .then(data =>{
+    let imagen= document.getElementById("imagen").src;
+    imagen.src=data.image;
+     let gender=data.gender;
+    
+        if (gender === "unknown"){
+            document.getElementById("result").innerHTML=`<img src="${data.image}">
+                                                         <p>${data.name}<p>
+                                                         <p>${data.gender}<p>`
+    
+        }
+            else {
+            
+                document.getElementById("result").innerHTML=("El personaje no es del genero que buscas")   
+            }
+    
+        console.log(data)   
+    } )
+    }
+*/
+
+
+function comparar(){
+    let codigoInput = document.getElementById("codigo").value;
+    let codigoInput2= document.getElementById("codigo2").value;
+    
+    fetch(`https://rickandmortyapi.com/api/character/${codigoInput},${codigoInput2}`)
+    .then(Response => Response.json())
+    .then(data =>{ 
+
+    let foto= document.getElementById("foto");
+    let foto2= document.getElementById("foto2");
+    
+    foto.src = data[0].image;
+    foto2.src = data[1].image;
+        
+    if (data[0].gender === data[1].gender) {
+        
+        document.getElementById("funcion").innerHTML=("Son del mismo genero")
+    } else
+        {
+            document.getElementById("funcion").innerHTML=("Su genero es distinto")
+        }
+
+
+
+
+    })}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //  console.log(personaje)
