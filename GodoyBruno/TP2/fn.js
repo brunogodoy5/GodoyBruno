b2d6eb175a1c7321ea51acd9c70ab057495d0df6
brunogodoy5 <130806880+brunogodoy5@users.noreply.github.com>
2024-04-25 21:24:59 -0300

    Add files via upload

---
+++ GodoyBruno/TP2/fn.js
@@ -0,0 +1,22 @@
+let frutas =['pera','manzana','frutilla','naranja','banana'];
+
+/*
+console.log($array[1])
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i])
+}  
+*/
+
+/*
+frutas.forEach(fruta => {
+    console.log(fruta)
+});
+*/
+
+
+/*for(const fruta of frutas) {
+    if (fruta=='manzana') {
+        console.log(fruta)
+    }
+    //console.log(fruta)
+}*/
